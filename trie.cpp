3b9c93668f494cb4327d4de52e199c2b349fe0e7
yuxintong12 <1517637121@qq.com>
2023-02-20 13:34:47 +0800

    lll

---
+++ trie.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+const int N = 100010;
+int tree[N][26],cnt[N];
+char put[N];
+int main()
+{
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        char op[2];
+        scanf("%s%s",op,put);
+        if(op[1] == )
+    }
+}
